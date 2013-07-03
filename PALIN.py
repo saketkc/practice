0146d5dc1984906cf75c6e385d08f4fca48c81ad
Saket Choudhary <saketkc@gmail.com>
2013-07-03 14:26:20 +0530

    new changes

--- PALIN.py
+++ PALIN.py
@@ -1,17 +1,38 @@
-def nextPalindrome(n):
-    a=True
-    while a:
-        if str(n)==str(n)[::-1]:
-            return n
-            a=False
-
+def main(n):
+    l=len(n)
+    c=l/2
+    if l%2:
+        left= n[:c]
+        center=(n[c])
+        right=n[c+1:]
+        if right<left:
+            palindrome=left+center+right[:-1]+left[0]
+            return palindrome
         else:
-            n+=1
+            center=int(center)
+            if center<9:
+                return left+(str(int(center)+1))+left[::-1]
+            else:
+                leftnew=str(int(left+1))
+                return leftnew+str(0)+str(int())+leftnew[::-1]
+    else:
+        left=n[:c]
+        right=n[c:]
+        leftreverse=left[::-1]
+        if leftreverse>right:
+            return left+leftreverse
+        else:
+            leftnew = str(int(left)+1)
+            return leftnew+leftnew[::-1]
+
+
+
+
 
 
 n=input()
 while n:
-    print nextPalindrome(input())
+    print main(raw_input())
     n=n-1
 
 
