86475913d7afa956f35c66c09a2243847152b614
Saket Choudhary <saketkc@gmail.com>
2013-07-15 10:31:39 +0530

    algo

---
+++ inversions.py
@@ -0,0 +1,33 @@
+def merge_and_count(a, b):
+    assert a == sorted(a) and b == sorted(b)
+    c = []
+    count = 0
+    i, j = 0, 0
+    while i < len(a) and j < len(b):
+        c.append(min(b[j], a[i]))
+        if b[j] < a[i]:
+            count += len(a) - i # number of elements remaining in `a`
+            j+=1
+        else:
+            i+=1
+    # now we reached the end of one the lists
+    c += a[i:] + b[j:] # append the remainder of the list to C
+    return count, c
+
+def sort_and_count(L):
+    if len(L) == 1: return 0, L
+    n = len(L) // 2 
+    a, b = L[:n], L[n:]
+    ra, a = sort_and_count(a)
+    rb, b = sort_and_count(b)
+    r, L = merge_and_count(a, b)
+    return ra+rb+r, L
+
+a=[]
+handle=open("/home/saket/Desktop/IntegerArray.txt","r")
+reader=handle.read()
+for lines in reader.splitlines():
+	
+	a.append(int(lines))
+c,d=sort_and_count(a)
+print c
---
+++ round.py
@@ -0,0 +1,6 @@
+import sys
+def roundtotens(num):
+    return (float(int(num*10+0.5))/10)
+print roundtotens(10)
+print roundtotens(8.62)
+print roundtotens(6.65)
