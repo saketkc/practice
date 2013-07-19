69b88caca9bf151628da7f4bf1ae0692bd04ea37
Saket Choudhary <saketkc@gmail.com>
2013-07-19 23:42:12 +0530

    dump all hw1

--- hw1.py
+++ hw1.py
@@ -25,8 +25,8 @@ p2_v_plus_two_u = [0,1,7]
 
 ## Problem 3
 # Write your answer using GF2's one instead of the number 1
-p3_vector_sum_1 = []
-p3_vector_sum_2 = [...]
+p3_vector_sum_1 = [one,0,0]
+p3_vector_sum_2 = [0,one,one]
 
 
 
@@ -36,35 +36,35 @@ p3_vector_sum_2 = [...]
 #   a (1100000), b (0110000), and c (0011000).
 # Leave an empty set if it cannot be expressed in terms of the other vectors.
 
-u_0010010 = ...
-u_0100010 = ...
+u_0010010 = {'c','d','e'}
+u_0100010 = {'b','c','d','e'}
 
 
 
 ## Problem 5
 # Use the same format as the previous problem
 
-v_0010010 = ...
-v_0100010 = ...
+v_0010010 = {'c','d'}
+v_0100010 = set()
 
 
 
 ## Problem 6
-uv_a = ...
-uv_b = ...
-uv_c = ...
-uv_d = ...
+uv_a = 5
+uv_b = 6 
+uv_c =  16
+uv_d = -1
 
 
 
 ## Problem 7
 # use 'one' instead of '1'
-x_gf2 = [...]
+x_gf2 = [one,0,0,0]
 
 
 
 ## Problem 8
-v1 = [...]
-v2 = [...]
-v3 = [...]
+v1 = [2,3,-4,1]
+v2 = [1,-5,2,0]
+v3 = [4,1,-1,-1]
 
